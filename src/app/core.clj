4ad815d0d907ce67fa8dc9bb99491198d42a0e01
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2015-01-09 14:57:12 +0100

    Add outcomes from results section
     - The ones in protocol can differ; TODO how to handle that.

--- src/app/core.clj
+++ src/app/core.clj
@@ -6,6 +6,17 @@
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
+(defn spo-each [subj pred obj*]
+  (reduce (fn [subj obj] (trig/spo subj [pred obj])) subj obj*))
+
+(defn outcome-rdf
+  [xml]
+  (let [uri (trig/iri :instance (uuid))]
+    [uri (trig/spo uri
+                   [(trig/iri :rdf "type") (trig/iri :ontology "Endpoint")]
+                   [(trig/iri :rdfs "label") (trig/lit (vtd/text (vtd/at xml "./title")))])]))
+
+
 (defn ctgov-import
   [xml]
   (let [prefixes {:rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
@@ -18,10 +29,17 @@
                   :instance "http://trials.drugis.org/instances/"
                   :entity "http://trials.drugis.org/entities/"
                   :dc "http://purl.org/dc/elements/1.1/"}
-        triples [(trig/spo (trig/iri :study (vtd/text (vtd/at xml "/clinical_study/id_info/nct_id")))
-                           [(trig/iri :rdf "type") (trig/iri :ontology "Study")]
-                           [(trig/iri :rdfs "label") (trig/lit (vtd/text (vtd/at xml "/clinical_study/brief_title")))]
-                           [(trig/iri :rdfs "comment") (trig/lit (vtd/text (vtd/at xml "/clinical_study/official_title")))])]]
+        uri (trig/iri :study (vtd/text (vtd/at xml "/clinical_study/id_info/nct_id")))
+        outcome-xml (vtd/search xml "/clinical_study/clinical_results/outcome_list/outcome")
+        outcomes (into {} (map outcome-rdf outcome-xml))
+        outcomes-rdf (vals outcomes)
+        outcomes-uri (keys outcomes)
+        study-rdf (-> uri
+                     (trig/spo [(trig/iri :rdf "type") (trig/iri :ontology "Study")]
+                               [(trig/iri :rdfs "label") (trig/lit (vtd/text (vtd/at xml "/clinical_study/brief_title")))]
+                               [(trig/iri :rdfs "comment") (trig/lit (vtd/text (vtd/at xml "/clinical_study/official_title")))])
+                     (spo-each (trig/iri :ontology "has_outcome") outcomes-uri))
+        triples (cons study-rdf outcomes-rdf)]
     (trig/write-ttl prefixes triples)))
 
 (defn -main
